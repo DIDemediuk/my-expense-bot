73d8f35940abc2a4d6eda37c2a8815f947d6dc97
Dmytro Demediuk <demediukdi@gmail.com>
2025-12-03 15:51:29 +0200

    Add dividends handling with name, category and FOP selection

---
+++ MPLABXProjects/Delonghi.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Delonghi.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Delonghi.X.production.hex
